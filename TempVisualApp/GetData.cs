14cad00f774f8cb531cc8068f7dd98ceb9ac8dcf
SMS <syu9799@woori-system.com>
2025-04-08 10:07:13 +0900

    Source Code Upload

---
+++ TempVisualApp/GetData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestProject
+{
+    public class GetData
+    {
+        public string meter_;
+        public float temp_;
+        public int left_;
+        public int top_;
+
+        public GetData(string[] data)    // 배열 받아와서 형변환 후 저장
+        {
+            meter_ = data[0].Replace('M', ' ');
+            temp_ = Convert.ToSingle(data[1]);
+            left_ = Convert.ToInt32(data[2]);
+            top_ = Convert.ToInt32(data[3]);
+        }
+    }
+}
---
+++ TempVisualApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace TestProject
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            StreamReader sr = new StreamReader("Cell.csv");  
+
+            while (!sr.EndOfStream)     // 내용이 없을 때까지 실행
+            {
+                string line = sr.ReadLine();     // 한 줄씩 읽어 line 에 저장
+                string[] data = line.Split(',');  // ',' 을 제거하고 배열에 저장
+
+                GetData gData = new GetData(data);      
+                GetColor gColor = new GetColor(gData.temp_);
+
+                Ellipse ell = new Ellipse()             // 원 생성
+                {
+                    Width = 20,
+                    Height = 20,
+                    Fill = new SolidColorBrush(gColor.color_)
+                };
+                Canvas.SetLeft(ell, gData.left_);   
+                Canvas.SetTop(ell, gData.top_);
+                myCanvas.Children.Add(ell);    // 원을 Canvas에 추가
+                ell.ToolTip = $"meter : {gData.meter_} temp : {gData.temp_}";  // 말풍선
+            }
+        }
+    }
+}
